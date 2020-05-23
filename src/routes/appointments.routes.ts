a819b24088ed2d5d78f96cb01a2567b7120c3e21
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2020-05-23 12:10:13 -0400

    GoBarber - Validando a data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,35 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments: { id: string; provider: string; date: string }[] = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
+  const parseDate = startOfHour(parseISO(date));
+  const findAppointmentSomeTime = appointments.find(appointment =>
+    isEqual(appointment.date, parseDate),
+  );
+  if (findAppointmentSomeTime) {
+    return res
+      .status(400)
+      .json({ message: "There's already a schedule at that time" });
+  }
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parseDate,
   };
   appointments.push(appointment);
 
-  return res.json(appointments);
+  return res.json(appointment);
 });
 
 export default appointmentsRouter;
