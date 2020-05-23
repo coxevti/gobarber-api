0f4afeb0239ed7c242e167e545ccab9476662659
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2020-05-23 08:45:58 -0400

    Debugando NodeJS

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello Word' });
+routes.post('/users', (req, res) => {
+  const { name, email } = req.body;
+  const user = { name, email };
+  res.json(user);
 });
 
 export default routes;
