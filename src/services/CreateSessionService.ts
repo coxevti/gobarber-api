5d5926a1e23560247bf3809b0504d6b91e3e7394
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2020-05-24 21:06:14 -0400

    GoBarber - Gerando token JWT

--- src/services/CreateSessionService.ts
+++ src/services/CreateSessionService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../entities/User';
 
 interface Request {
@@ -7,8 +8,13 @@ interface Request {
   password: string;
 }
 
+interface Response {
+  user: User;
+  token: string;
+}
+
 class CreateSessionService {
-  public async execute({ email, password }: Request): Promise<User> {
+  public async execute({ email, password }: Request): Promise<Response> {
     const userRepository = getRepository(User);
     const user = await userRepository.findOne({
       where: { email },
@@ -20,7 +26,12 @@ class CreateSessionService {
     if (!checkPassword) {
       throw new Error('The email or password you entered is incorrect.');
     }
-    return user;
+    const token = sign(
+      {},
+      '220de20c0f928f20282338a09cdfdc0db9e3f88ec7bda96ffb2adbfd9d6498c8',
+      { subject: user.id, expiresIn: '1d' },
+    );
+    return { user, token };
   }
 }
 
